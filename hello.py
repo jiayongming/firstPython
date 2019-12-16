2455b2491e76e43ceb5e2f60b814c14a0ade3606
ymjia <ymjia@rxhui.com>
2019-12-16 15:28:17 +0800

    hello word！

--- hello.py
+++ hello.py
@@ -2,3 +2,25 @@
 # coding=utf-8
 
 print ("你好，世界")
+print ('hello');print ('runoob')
+
+# 我是一个注释
+if False:
+    print ("Answer")
+    print ("True")
+else:
+    print ("Answer")
+    # 没有严格缩进，在执行时会报错
+    print ("False")
+
+'''
+这是多行注释，使用单引号。
+这是多行注释，使用单引号。
+这是多行注释，使用单引号。
+'''
+
+"""
+这是多行注释，使用双引号。
+这是多行注释，使用双引号。
+这是多行注释，使用双引号。
+"""
